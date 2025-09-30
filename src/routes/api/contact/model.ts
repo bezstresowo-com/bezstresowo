4ced0cdd43777542a4fe18b5ef544d73651c8462
Szymon Kaszuba-Gałka <szymonkaszuba3@wp.pl>
2025-09-30 20:14:43 +0200

    wip

--- src/routes/api/contact/model.ts
+++ src/routes/api/contact/model.ts
@@ -1,36 +1,39 @@
+import { VALIDATION_ERRORS_TRANSLATION_PREFIX } from '$shared/server/consts';
 import { IsDefined, IsEmail, IsPhoneNumber, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class ContactRequestDto {
-	@IsDefined({ message: 'api.validation.errors.IsDefined' })
-	@IsString({ message: 'api.validation.errors.IsString' })
-	@MinLength(1, { message: 'api.validation.errors.MinLength@{"length":1}' })
-	@MaxLength(100, { message: 'api.validation.errors.MaxLength@{"length":100}' })
+	@IsDefined({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsDefined` })
+	@IsString({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsString` })
+	@MinLength(1, { message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.MinLength@{"length":1}` })
+	@MaxLength(100, { message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.MaxLength@{"length":100}` })
 	name: string;
 
-	@IsDefined({ message: 'api.validation.errors.IsDefined' })
-	@IsString({ message: 'api.validation.errors.IsString' })
-	@MinLength(1, { message: 'api.validation.errors.MinLength@{"length":1}' })
-	@MaxLength(100, { message: 'api.validation.errors.MaxLength@{"length":100}' })
+	@IsDefined({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsDefined` })
+	@IsString({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsString` })
+	@MinLength(1, { message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.MinLength@{"length":1}` })
+	@MaxLength(100, { message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.MaxLength@{"length":100}` })
 	surname: string;
 
-	@IsDefined({ message: 'api.validation.errors.IsDefined' })
-	@IsString({ message: 'api.validation.errors.IsString' })
-	@IsPhoneNumber(undefined, { message: 'api.validation.errors.IsPhoneNumber' })
+	@IsDefined({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsDefined` })
+	@IsString({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsString` })
+	@IsPhoneNumber(undefined, { message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsPhoneNumber` })
 	tel: string;
 
-	@IsDefined({ message: 'api.validation.errors.IsDefined' })
-	@IsEmail(undefined, { message: 'api.validation.errors.IsEmail' })
+	@IsDefined({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsDefined` })
+	@IsEmail(undefined, { message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsEmail` })
 	email: string;
 
-	@IsDefined({ message: 'api.validation.errors.IsDefined' })
-	@IsString({ message: 'api.validation.errors.IsString' })
-	@MinLength(10, { message: 'api.validation.errors.MinLength@{"length":10}' })
-	@MaxLength(150, { message: 'api.validation.errors.MaxLength@{"length":150}' })
+	@IsDefined({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsDefined` })
+	@IsString({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsString` })
+	@MinLength(10, { message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.MinLength@{"length":10}` })
+	@MaxLength(150, { message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.MaxLength@{"length":150}` })
 	subject: string;
 
-	@IsDefined({ message: 'api.validation.errors.IsDefined' })
-	@IsString({ message: 'api.validation.errors.IsString' })
-	@MinLength(10, { message: 'api.validation.errors.MinLength@{"length":10}' })
-	@MaxLength(10_000, { message: 'api.validation.errors.MaxLength@{"length":10000}' })
+	@IsDefined({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsDefined` })
+	@IsString({ message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.IsString` })
+	@MinLength(10, { message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.MinLength@{"length":10}` })
+	@MaxLength(10_000, {
+		message: `${VALIDATION_ERRORS_TRANSLATION_PREFIX}.MaxLength@{"length":10000}`
+	})
 	message: string;
 }
